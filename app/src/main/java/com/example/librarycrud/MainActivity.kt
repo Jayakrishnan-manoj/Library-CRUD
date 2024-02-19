7066ed9f66f4b128fdca0e8c673a60a087e937a2
jk.jayakrishnan27 <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2024-02-19 23:16:50 +0530

    create: book entity and main screen

--- app/src/main/java/com/example/librarycrud/MainActivity.kt
+++ app/src/main/java/com/example/librarycrud/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.librarycrud
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.librarycrud.ui.theme.LibraryCRUDTheme
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,17 +34,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    Column (horizontalAlignment = Alignment.CenterHorizontally){
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Book Name") },
+            placeholder = { Text(text = "Enter Book Name") },
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LibraryCRUDTheme {
-        Greeting("Android")
+        )
+
+        Button(onClick = {}) {
+            Text(text = "Insert Book into DB")
+        }
     }
 }
---
+++ app/src/main/java/com/example/librarycrud/room/BookEntity.kt
@@ -0,0 +1,13 @@
+package com.example.librarycrud.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class BookEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+
+    val title: String
+
+)
